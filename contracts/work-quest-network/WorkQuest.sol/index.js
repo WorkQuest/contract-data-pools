6170ae673cd6c7d0f0dca01f0da91dd95ebdc926
VasilijS <catred@mail2000.ru>
2022-05-30 13:45:00 +0700

    fix deployment hight for quests

--- contracts/work-quest-network/WorkQuest.sol/index.js
+++ contracts/work-quest-network/WorkQuest.sol/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const abiPath = path.join(__dirname, '/abi/WorkQuest.json');
 
 module.exports = {
-  deploymentHeight: 200,
+  deploymentHeight: 30311,
   address: '0x0000',
   getAbi() {
     const abiFile = fs.readFileSync(abiPath).toString();
