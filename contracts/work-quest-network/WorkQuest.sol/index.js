5e46100e402ad27ad7350e0e0db258d5d6affa22
VasilijS <catred@mail2000.ru>
2022-05-30 13:28:10 +0700

    fix quests block hight

--- contracts/work-quest-network/WorkQuest.sol/index.js
+++ contracts/work-quest-network/WorkQuest.sol/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const abiPath = path.join(__dirname, '/abi/WorkQuest.json');
 
 module.exports = {
-  deploymentHeight: 200,
+  deploymentHeight: 13433,
   address: '0x0000',
   getAbi() {
     const abiFile = fs.readFileSync(abiPath).toString();
