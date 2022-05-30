a0c1331576b37ef3caffeb7987261d6784239f61
VasilijS <catred@mail2000.ru>
2022-05-30 16:32:22 +0700

    add bridge for usdt

---
+++ contracts/bnb-network/WQBridgeStable.sol/index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const abiPath = path.join(__dirname, '/abi/WQBridgeStable.json');
+
+module.exports = {
+    deploymentHeight: 18832470,
+    address: '0x833d71EF0b51Aa9Fb69b1f986381132628ED10F3',
+    getAbi() {
+        const abiFile = fs.readFileSync(abiPath).toString();
+
+        return JSON.parse(abiFile);
+    }
+}
---
+++ contracts/eth-network/WQBridgeStable.sol/index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+
+const abiPath = path.join(__dirname, '/abi/WQBridgeStable.json');
+
+module.exports = {
+    deploymentHeight: 10581406,
+    address: '0x9870a749Ae5CdbC4F96E3D0C067eB212779a8FA1',
+    getAbi() {
+        const abiFile = fs.readFileSync(abiPath).toString();
+
+        return JSON.parse(abiFile);
+    }
+}
