72088131f52ffc2634bdaa950d5ee3c3c912e59a
VasilijS <catred@mail2000.ru>
2022-06-30 16:39:07 +0700

    deploy price oracle

--- contracts/work-quest-network/WQPriceOracle.sol/index.js
+++ contracts/work-quest-network/WQPriceOracle.sol/index.js
@@ -4,8 +4,8 @@ const path = require('path');
 const abiPath = path.join(__dirname, '/abi/WQPriceOracle.json');
 
 module.exports = {
-  deploymentHeight: 0,
-  address: '0x0000000000000000000000000000000000000000',
+  deploymentHeight: 95279,
+  address: '0x120181418cF8E658D7036EF7cA450B73c8Df7CB9',
   getAbi() {
     const abiFile = fs.readFileSync(abiPath).toString();
 
