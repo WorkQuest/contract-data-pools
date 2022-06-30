2a31368a6adaef33799178c546aee396a5148979
VasilijS <catred@mail2000.ru>
2022-06-30 16:42:16 +0700

    deploy referral

--- contracts/work-quest-network/WQReferral.sol/index.js
+++ contracts/work-quest-network/WQReferral.sol/index.js
@@ -4,8 +4,8 @@ const path = require('path');
 const abiPath = path.join(__dirname, '/abi/WQReferral.json');
 
 module.exports = {
-  deploymentHeight: 0,
-  address: '0x0000000000000000000000000000000000000000',
+  deploymentHeight: 95363,
+  address: '0x059c1c966E8f1D74e15746A06a6DD074FBB01a4f',
   getAbi() {
     const abiFile = fs.readFileSync(abiPath).toString();
 
