54d3d4658241738e3a3e59990e27b29dd49a4383
VasilijS <catred@mail2000.ru>
2022-07-18 14:33:51 +0700

    fix address of oracle

--- contracts/work-quest-network/WQPriceOracle.sol/index.js
+++ contracts/work-quest-network/WQPriceOracle.sol/index.js
@@ -4,8 +4,8 @@ const path = require('path');
 const abiPath = path.join(__dirname, '/abi/WQPriceOracle.json');
 
 module.exports = {
-  deploymentHeight: 1182726,
-  address: '0x087925f8d239435212deeB090EBA074bcCAF0D4C',
+  deploymentHeight: 1182728,
+  address: '0x2Bf4DEB0d1dDcd19E3561340FCbC9607E00273a7',
   getAbi() {
     const abiFile = fs.readFileSync(abiPath).toString();
 
