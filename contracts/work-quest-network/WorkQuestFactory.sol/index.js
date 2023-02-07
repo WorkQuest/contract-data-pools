ab8013d2ffdc83bcb62201ec4b390ec3f3c630fd
Fw-White-Rabbit <124710268+Fw-White-Rabbit@users.noreply.github.com>
2023-02-08 00:22:40 +0700

    Update

--- contracts/work-quest-network/WorkQuestFactory.sol/index.js
+++ contracts/work-quest-network/WorkQuestFactory.sol/index.js
@@ -5,7 +5,7 @@ const abiPath = path.join(__dirname, '/abi/WorkQuestFactory.json');
 
 module.exports = {
   deploymentHeight: 1430456,
-  address: '0xC16F2efbE0FF219d3c60bF9B1fF088c679d73D79',
+  address: '0x6E15Be0fFf8357C2A03C8918637b2FeEa2831b5F',
   getAbi() {
     const abiFile = fs.readFileSync(abiPath).toString();
 
