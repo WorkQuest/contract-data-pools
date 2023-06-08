a867c3bfac87ce9818306ac2d8fc6c9bf3b5dc6f
Fw-White-Rabbit <124710268+Fw-White-Rabbit@users.noreply.github.com>
2023-06-09 00:13:29 +0700

    update

--- contracts/bnb-network/WqtWbnb.sol/index.js
+++ contracts/bnb-network/WqtWbnb.sol/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const abiPath = path.join(__dirname, '/abi/WqtWbnb.json');
 
 module.exports = {
-    deploymentHeight: 1430456,
+    deploymentHeight: 28715056,
     address: '0xdB53703E7998441d93794cb0AF1092Fac2778124',
     getAbi() {
         const abiFile = fs.readFileSync(abiPath).toString();
