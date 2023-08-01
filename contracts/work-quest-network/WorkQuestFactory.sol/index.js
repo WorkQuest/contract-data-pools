570194f76893e911f9d217d02c703e9519b619a8
Fw-White-Rabbit <follow.white_rabbit@yahoo.com>
2023-08-01 23:57:30 +0700

    update quest factory

--- contracts/work-quest-network/WorkQuestFactory.sol/index.js
+++ contracts/work-quest-network/WorkQuestFactory.sol/index.js
@@ -5,7 +5,7 @@ const abiPath = path.join(__dirname, '/abi/WorkQuestFactory.json');
 
 module.exports = {
   deploymentHeight: 95718,
-  address: '0x3d9782B4Ba9C10d09973dd1f7C16410c931f5468',
+  address: '0x0ccC76540E087b2E7567F7BFf80d7EEA0d4F00aC',
   getAbi() {
     const abiFile = fs.readFileSync(abiPath).toString();
 
