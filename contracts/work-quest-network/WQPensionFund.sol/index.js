f4b3f2325a44cf073e06a99c72f7eff2f315b987
Fw-White-Rabbit <follow.white_rabbit@yahoo.com>
2023-08-04 01:36:41 +0700

    update

--- contracts/work-quest-network/WQPensionFund.sol/index.js
+++ contracts/work-quest-network/WQPensionFund.sol/index.js
@@ -5,7 +5,7 @@ const abiPath = path.join(__dirname, '/abi/WQPensionFund.json');
 
 module.exports = {
   deploymentHeight: 95407,
-  address: '0x527aC80974c66939cBf686648064846708234256',
+  address: '0x937F7125994a91d5E2Ce31846b97578131056Bb4',
   getAbi() {
     const abiFile = fs.readFileSync(abiPath).toString();
 
--- contracts/work-quest-network/WQReferral.sol/index.js
+++ contracts/work-quest-network/WQReferral.sol/index.js
@@ -5,7 +5,7 @@ const abiPath = path.join(__dirname, '/abi/WQReferral.json');
 
 module.exports = {
   deploymentHeight: 95363,
-  address: '0x059c1c966E8f1D74e15746A06a6DD074FBB01a4f',
+  address: '0x9618E57fe0a3b3A79723CaC7c6AEF330C3Df515E',
   getAbi() {
     const abiFile = fs.readFileSync(abiPath).toString();
 
